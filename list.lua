b5d0fe9da46ae9b461d87ec2fbcf3b2859075938
CuneoTop <154107438+CuneoTop@users.noreply.github.com>
2025-04-19 01:58:14 +0800

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,35 +1,48 @@
 -- Admin KemilingHUB
-Usn: HaiseSasaki242424 | Key: 1111 | Exp: 01/01/3000
-Usn: venzrazel | Key: 1111 | Exp: 01/01/3000
-Usn: 1 | Key: 1111 | Exp: 01/01/3000
+KemilingHUB = {
+    {Usn = "venzrazel", Key = "1111", Exp = "01/01/3000"},
+    {Usn = "1", Key = "1111", Exp = "01/01/3000"}
+}
 
 -- Free (Premium) User
-Usn: DanzzRBX | Key: 102009 | Exp: 01/01/3000
-Usn: Sad_FahriINDO | Key: wD29U65n9e | Exp: 10/04/2025
+PremiumUsers = {
+    {Usn = "DanzzRBX", Key = "102009", Exp = "01/01/3000"},
+    {Usn = "Sad_FahriINDO", Key = "wD29U65n9e", Exp = "10/04/2025"}
+}
 
 -- Buyer Ingame
-Usn: sevshtz | Key: jxXepQ5XR | Exp: 01/04/2025 12:57
+BuyerIngame = {
+    {Usn = "sevshtz", Key = "jxXepQ5XR", Exp = "01/04/2025 12:57"}
+}
 
 -- Nbuy User
-Usn: usrenamea | Nbuy: true
+NbuyUser = {
+    {Usn = "usrenamea", Nbuy = true}
+}
 
 -- Note User
-Usn: ATHONKGAMING | Key: ucNXxhceLy | Note: JAWA JAWA JAWA
+NoteUser = {
+    {Usn = "ATHONKGAMING", Key = "ucNXxhceLy", Note = "JAWA JAWA JAWA"}
+}
 
 -- Blacklist
-Usn: DODIKANJUTT | Blacklist: true
+Blacklist = {
+    {Usn = "DODIKANJUTT", Blacklist = true}
+}
 
 -- Blacklist (Admin Ingame)
-Usn: niohailhad | Blacklist: true
-Usn: Emceeee0 | Blacklist: true
-Usn: dizon_3490 | Blacklist: true
-Usn: vj101yt | Blacklist: true
-Usn: MrDash_68 | Blacklist: true
-Usn: IInsaneStorage | Blacklist: true
-Usn: yuhanforyoupls | Blacklist: true
-Usn: kyrziis | Blacklist: true
-Usn: KingSnoopJay | Blacklist: true
-Usn: Nel_cookie | Blacklist: true
-Usn: todoroki_kudo | Blacklist: true
-Usn: DuminuStudio | Blacklist: true
-Usn: xxrosero09 | Blacklist: true
+AdminBlacklist = {
+    {Usn = "niohailhad", Blacklist = true},
+    {Usn = "Emceeee0", Blacklist = true},
+    {Usn = "dizon_3490", Blacklist = true},
+    {Usn = "vj101yt", Blacklist = true},
+    {Usn = "MrDash_68", Blacklist = true},
+    {Usn = "IInsaneStorage", Blacklist = true},
+    {Usn = "yuhanforyoupls", Blacklist = true},
+    {Usn = "kyrziis", Blacklist = true},
+    {Usn = "KingSnoopJay", Blacklist = true},
+    {Usn = "Nel_cookie", Blacklist = true},
+    {Usn = "todoroki_kudo", Blacklist = true},
+    {Usn = "DuminuStudio", Blacklist = true},
+    {Usn = "xxrosero09", Blacklist = true}
+}
